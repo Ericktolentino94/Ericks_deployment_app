33dd488db55d3001ddf034548873f86de3af790d
Erick Tolentino <ericktolentino@pursuit.org>
2023-07-26 20:55:02 -0400

    added the css lab

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,59 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Erick</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+       <h1>Erick Tolentino</h1>
+    <div>
+      <p>
+        This is a bunch of information about myself. I'm from here and there and
+        discovered my love of programming when this happened. When I'm not
+        working I'm busy doing this and that.
       </p>
+    </div>
+
+    <img
+      src= "https://cdn.britannica.com/60/217660-050-DBCC409A/cheddar-cheese-wedge.jpg"
+      alt=""
+    />
+
+    <div>
+      <h3>Here are some of my Skills!</h3>
+      <ul>
+        <h3>Languages</h3>
+        <li>JavaScript</li>
+        <li>SQL</li>
+        <li>HTML5</li>
+        <li>CSS3</li>
+      </ul>
+
+      <ol>
+        <h3>Libraries</h3>
+        <li>React</li>
+        <li>PostgreSQL</li>
+        <li>Node</li>
+        <li>Bootstrap</li>
+      </ol>
+    </div>
+
+    <div>
+      <h3>Hardest Bug So Far</h3>
+      <p>
+        My hardest bug I ever came across was this infinite loop I couldn't
+        escape.
+      </p>
+      <p>I came up with a totally sick solution though by doing ...</p>
+    </div>
+
+    <h2>Contact Me</h2>
+    <div>
+      Email me at:
+      <a href="mailto:hello@pursuit.org" target="_top">hello@pursuit.org</a>
+    </div>
+
+    <ul>
+      <li><a href="github.com">github link</a></li>
+      <li><a href="linkedin.com">LinkedIn link</a></li>
+      <li><a href="angellist.com">Angel list link</a></li>
+    </ul>
     </>
   )
 }
